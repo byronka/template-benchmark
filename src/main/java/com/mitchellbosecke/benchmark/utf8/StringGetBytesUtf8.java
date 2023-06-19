5752c89cab4cdf2fe31ddccf700c3a667756879f
Adam Gent <adam.gent@snaphop.com>
2023-06-19 19:38:51 -0400

    Call render directly to avoid method invocations

--- src/main/java/com/mitchellbosecke/benchmark/utf8/StringGetBytesUtf8.java
+++ src/main/java/com/mitchellbosecke/benchmark/utf8/StringGetBytesUtf8.java
@@ -11,6 +11,10 @@ import com.mitchellbosecke.benchmark.JStachio.StocksModel;
 import com.mitchellbosecke.benchmark.JStachioStocksTemplate;
 import com.mitchellbosecke.benchmark.model.Stock;
 
+import io.jstach.jstachio.Appender;
+import io.jstach.jstachio.Escaper;
+import io.jstach.jstachio.Formatter;
+import io.jstach.jstachio.Output;
 import io.jstach.jstachio.escapers.Html;
 import io.jstach.jstachio.formatters.DefaultFormatter;
 
@@ -19,17 +23,22 @@ public class StringGetBytesUtf8 extends BaseBenchmark {
     private List<Stock> items;
     private StocksModel model;
     private JStachioStocksTemplate template;
+    private Formatter formatter = DefaultFormatter.of();
+    private Escaper escaper = Html.provider();
+    private Appender appender = Appender.defaultAppender();
     
     @Setup
     public void setup() {
         items = Stock.dummyItems();
         model = new StocksModel(items);
-        template = new JStachioStocksTemplate(DefaultFormatter.provider(), Html.provider());
+        template = new JStachioStocksTemplate(formatter, escaper);
     }
 
     @Benchmark
     public byte[] benchmark() {
         StringBuilder sb = new StringBuilder(8 * 1024);
-        return template.execute(model, sb).toString().getBytes(StandardCharsets.UTF_8);
+        JStachioStocksTemplate.render(model, Output.of(sb), formatter, escaper, appender);
+        //return template.execute(model, sb).toString().getBytes(StandardCharsets.UTF_8);
+        return sb.toString().getBytes(StandardCharsets.UTF_8);
     }
 }
