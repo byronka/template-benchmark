de3be98203cea2e0f2d50d6af990b0e7b25774a0
Adam Gent <adam.gent@snaphop.com>
2023-06-30 15:15:20 -0400

    Reduce heap size for windows

--- utf8.sh
+++ utf8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #CMD ["java", "-server", "-Xms4g", "-Xmx4g", "-XX:-UseBiasedLocking", "-XX:+UseStringDeduplication", "-XX:+UseNUMA", "-XX:+UseParallelGC", "-jar", "app.jar"] 
 
-JVM_ARGS="-server -Xms4g -Xmx4g -XX:+UseStringDeduplication -XX:+UseNUMA -XX:+UseParallelGC"
+JVM_ARGS="-server -Xms2g -Xmx2g -XX:+UseStringDeduplication -XX:+UseNUMA -XX:+UseParallelGC"
 java ${JVM_ARGS} \
   -Dbenchmark.utf8=true -jar target/benchmarks.jar Utf8 -rff results-utf8.csv -rf csv
 
