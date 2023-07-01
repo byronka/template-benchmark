fd170fc743c8249ce164ddb88e6e0c7818b9da4f
Adam Gent <adam.gent@snaphop.com>
2023-07-01 11:55:40 -0400

    Try to fix windows gnuplot

--- gnuplot.sh
+++ gnuplot.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-if [ -x "$(command -v gnuplot)" ]; then
+
+case "$(uname -sr)" in
+
+   CYGWIN*|MINGW*|MINGW32*|MSYS*)
+	   _WINDOWS=true
+     ;;
+esac
+
+if [[ "$_WINDOWS" == "true" ]]; then
+ls /c/Program\ Files/gnuplot
+/c/Program\ Files/gnuplot/gnuplot $1
+elif [ -x "$(command -v gnuplot)" ]; then
 gnuplot $1
 else
 docker run --rm -v $(pwd):/work remuslazar/gnuplot $1
